75b06fd033e8341dc5bced19f1f1978376e68214
beata <beata@emc2wave.com>
2020-04-26 16:53:32 -0700

    add window.stuffLoaded = true;

--- samplewebproject/index.js
+++ samplewebproject/index.js
@@ -12,3 +12,5 @@ document.querySelector('form').addEventListener('submit', (e) => {
     header.innerHTML = 'Invalid email';
   }
 });
+
+window.stuffLoaded = true;
--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -19,5 +19,5 @@ it('shows a success message with a valid email', async () => {
 
   const h1 = dom.window.document.querySelector('h1');
 
-  console.log(h1.innerHTML);
+  console.log('Contents of h1', h1.innerHTML);
 });
